f4d04b77be4597bd2d13702694ca069a05fb446b
AkritiSubedi <aakusubedi075@gmail.com>
2023-11-05 16:42:51 +0545

    chapter2_completed

--- chapter2/variables.py
+++ chapter2/variables.py
@@ -5,6 +5,19 @@
 name = "Akriti Subedi"
 age = 21
 weight = 40.5
+d = None
+e = False
+
+# printing the variables
 print(name)
 print(age)
 print(weight)
+print(d)
+print(e)
+
+# printing thee types of variables
+print(type(age))
+print(type(name))
+print(type(weight))
+print(type(d))
+print(type(e))
