37fd4c58bf33f7442d5929ef0cf994abf85bdb30
AkritiSubedi <aakusubedi075@gmail.com>
2023-11-06 15:43:47 +0545

    solution

---
+++ chapter2/solution1.py
@@ -0,0 +1,17 @@
+# sum of two numbers
+
+a = 33
+b = 45
+print("The sum of a and b is", a + b)
+
+# to find a remainder when a is divided by 2
+
+print("The remainder When a is divided by b is", a% b) 
+
+
+# find a given number aa is greater than bb or not? 
+
+aa = 45
+bb = 56
+bb = aa > bb # aa is not greater than bb
+print(bb)
---
+++ chapter2/solution2.py
@@ -0,0 +1,13 @@
+# avg of two numbers entered by users
+a = input("Enter first number :")
+b = input("Enter second number:")
+a = int(a)
+b = int(b)
+avg = (a + b)/2
+print("The average of a and b is:", avg)
+
+
+# square of numbers entered by users
+c = 25
+c = int(c)
+print ("The square of number c is:", c*c)
