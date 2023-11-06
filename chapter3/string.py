ed825f972ba5311679bcbb5ecea5faf271403b62
AkritiSubedi <aakusubedi075@gmail.com>
2023-11-06 16:29:05 +0545

    chapter3_completed

---
+++ chapter3/string.py
@@ -0,0 +1,21 @@
+'''String is a data type in python.
+    We can primarily write a string in these three ways
+    1. single quoted strings i.e 'Akriti'
+    2. Double quoted strings i.e "Akriti"
+    3. Triple quoted strings 
+
+    '''
+
+greeting = "Good Morning, "
+name = "AKRITI"
+print(type(name))
+print(greeting + name)
+
+# finding length
+print(name[2])
+ # name[4] = "a" --> Does not work
+
+ #finding slicing
+print(name[2:6])
+print(name[-3:4])
+
---
+++ chapter3/string_functions.py
@@ -0,0 +1,16 @@
+poem = '''twinkle, twinkle, little star,
+    How I wonder what you are!
+    Up above the world so high,
+    Like a diamond in the sky.
+    Twinkle, twinkle, little star,
+    How I wonder what you are'''
+
+# string functions
+print(len(poem))
+print(poem.endswith('are'))
+print(poem.count("star"))
+print(poem.count("t"))
+print(poem.capitalize()) # this function capitalizes the first character of string
+print(poem.find("so"))
+print(poem.replace("star", "stars"))
+
