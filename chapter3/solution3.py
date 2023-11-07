f9532a2aafe054660100ba82aed63d361dd21f85
AkritiSubedi <aakusubedi075@gmail.com>
2023-11-07 16:30:58 +0545

    chapter3_completed

---
+++ chapter3/solution3.py
@@ -0,0 +1,6 @@
+# escape sequence characters
+python = "Python is a high-level, versatile, and interpreted programming language. It was created by Guido van Rossum and first released in 1991. "
+print(python)
+
+formatted_python = "Python \tis a high-level, versatile, and interpreted programming language. \nIt was created by Guido van Rossum and first released in 1991. "
+print(formatted_python)
---
+++ solution2.py
@@ -0,0 +1,10 @@
+# detect double spaces in a string
+st = "String with double   spaces"
+doubleSpaces = st.find(" ")
+print(doubleSpaces)
+
+# detect single spaces
+str = "String with double   spaces    Akriti"
+str = str.replace("  ", " ")
+print(str)
+
