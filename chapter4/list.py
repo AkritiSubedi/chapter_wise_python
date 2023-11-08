e4600b295d2272331608bfc6be6121f5a9eb7ce3
AkritiSubedi <aakusubedi075@gmail.com>
2023-11-08 14:33:57 +0545

    chapter4 completed

---
+++ chapter4/list.py
@@ -0,0 +1,21 @@
+# creating a list
+a = [1, 2, 4, 55, 100]
+
+# print the list using print() function
+print(a)
+
+# Access using index using a[0], a[1], a[2]
+print(a[2])
+
+# replacing a[3] as 105
+a[3] = 105
+print(a)
+
+# we can create a list with items of different types
+c = [45, "Harry", False, 6.4]
+print(c)
+
+
+
+
+
