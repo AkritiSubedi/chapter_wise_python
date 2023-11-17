57c3c06e9017dbe07df6c82e9bebd8c4c5d4fcfa
AkritiSubedi <aakusubedi075@gmail.com>
2023-11-17 14:06:26 +0545

    chapter7 completed

---
+++ chapter7/forloop_list.py
@@ -0,0 +1,9 @@
+fruits = ['Banana', 'Watermelon', 'Grapes','Apple','Mango']
+
+for item in fruits:
+    print(item)
+
+
+# range function
+for i in range(1, 8):
+    print(i)
---
+++ chapter7/while_loops.py
@@ -0,0 +1,14 @@
+i = 0
+while i<10:
+    print("Yes" + str(i))
+    i = i + 1
+
+print("Done")
+
+
+# program to print 1 to 50 using ehile loop
+j = 1
+
+while i<= 50:
+    print(i)
+    j = j + 1
