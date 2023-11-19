828fa853305cb4f39946f415c41107325557ee87
AkritiSubedi <aakusubedi075@gmail.com>
2023-11-19 16:28:43 +0545

    chapter8 completed

---
+++ chapter8/default_arguments.py
@@ -0,0 +1,9 @@
+''' if we specify name i.e stranger in the line containing def, 
+this value is used when no arguments is passed'''
+
+
+def greet(name = "Stranger"):
+    print("Good Day, "+ name)
+
+greet("Akriti")
+greet()
---
+++ chapter8/functions.py
@@ -0,0 +1,9 @@
+
+
+marks = [45, 78, 86, 77]
+percentage = ((marks[0] + marks[1] + marks[2] + marks[3])/400)*100
+
+marks2 = [75, 98, 100, 90]
+percentage2 = (sum(marks2)/400)*100
+
+print(percentage, percentage2)
